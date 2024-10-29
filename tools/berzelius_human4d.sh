ac1952e0b2627e270ca8be2a24ba447966c02fff
Georg Hess <georg.hess@zenseact.com>
2024-10-29 07:33:01 +0100

    more processing time

--- tools/berzelius_human4d.sh
+++ tools/berzelius_human4d.sh
@@ -2,7 +2,7 @@
 #
 #SBATCH --nodes 1
 #SBATCH --gpus 1
-#SBATCH --time 02:00:00
+#SBATCH --time 04:00:00
 #SBATCH --output /proj/agp/users/%u/logs/%j.out
 #SBATCH -A Berzelius-2024-322
 #SBATCH --job-name=human4d
