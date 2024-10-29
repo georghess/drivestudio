8ecb97cfdb70c64a45d720d72c624bbe10011c03
Georg Hess <georg.hess@zenseact.com>
2024-10-29 07:42:33 +0100

    config changes

--- tools/berzelius_array.sh
+++ tools/berzelius_array.sh
@@ -2,7 +2,7 @@
 #
 #SBATCH --nodes 1
 #SBATCH --gpus 1
-#SBATCH --time 03:00:00
+#SBATCH --time 04:00:00
 #SBATCH --output /proj/agp/users/%u/logs/%A_%a.out
 #SBATCH -A Berzelius-2024-322
 #SBATCH --array=1-10
